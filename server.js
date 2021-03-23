8d551d277a98409e9f24962f22075bbab66e857a
Nomin <knm@iansit.co.kr>
2021-03-23 19:14:27 +0900

    test

--- server.js
+++ server.js
@@ -88,6 +88,10 @@ models.sequelize
     logger.error("Connect Database Faild..");
     logger.error("Connect Database Faild..");
     logger.error("Connect Database Faild..");
+    logger.error("Connect Database Faild..");
+    logger.error("Connect Database Faild..");
+    logger.error("Connect Database Faild..");
+    logger.error("Connect Database Faild..");
     console.log(err);
   });
 
